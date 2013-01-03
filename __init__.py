f86fafa8ba42e0c1c28289aefca8703cb5566782
Erik Peterson <Erik.Exists@gmail.com>
2013-01-03 17:05:48 -0600

    stats.counts() and .group_counts() are tested.
    They now rely on pandas.Series.value_counts() as Counter could not handle the NAs present in the data.
    
    The resulting Series objects can be treated much like Counters could, it seems.

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 import convert
+import stats
--- stats.py
+++ stats.py
@@ -1,29 +1,31 @@
 """ A submodule for calculating basic statistics about .csv metadata files
 converted from fidl files.  To convert see fidl.convert.fidl_to_csv(). 
 
-Requires pandas (http://pandas.pydata.org/)
+Requires pandas (http://pandas.pydata.org/).
 """
 import os
-from collections import Counter
-import pandas as pan
+import pandas as pd
 
 
 def counts(csvfile, col):
     """ Return the counts for each unique entry in <col>. <col> can be a 
     string, i.e. the <col> name, or an integer (0...N-1), where N
-    is the number of cols. """
+    is the number of cols. 
+    
+    The returned counts are in a pandas.Series objects, which
+    can be treated a lot like a dictionary. """
     
     # Read in the csv
-    data = pd.read_csv(csvfile)
+    data = pd.read_csv(csvfile, na_values=['NA',])
     
     # And get the counts
     cnts = None
     try:
         # Try col as the name
-        cnts = Counter(data[col])
+        cnts = pd.Series.value_counts(data[col])
     except KeyError:
         # then as an index
-        cnts = Counter(data.ix[:,col])
+        cnts = pd.Series.value_counts(data.ix[:,col])
 
     return cnts
     
@@ -32,22 +34,18 @@ def group_counts(csvfiles, col):
     """ Return the *mean* counts for each unique entry in col 
     for all the files in <csvfiles>. <col> can be a 
     string, i.e. the <col> name, or an integer (0...N-1), where N
-    is the number of cols. """
+    is the number of cols.
     
-    # Use the first entry in csvfiles 
-    # to init groupcnt
+    The returned counts are in a pandas.Series objects, which
+    can be treated a lot like a dictionary. """
+    
     groupcnt = counts(csvfiles.pop(), col)
-    
     # Loop over csvfiles
     # adding cnts to groupcnt
-    # if there is an entry in cnts
-    # but not in groupcnt it will
-    # die with a KeyError, which
-    # is the desired outcome.
     for cf in csvfiles:
-        for k, v in counts(cf, col).items():
+        for k, v in counts(cf, col).iteritems():
             groupcnt[k] += v
-
+    
     return groupcnt
     
     
